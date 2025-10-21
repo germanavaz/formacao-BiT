976fe19ec0ac40de3ae5a72d64360e25da089a7c
germanavaz <gmsvaz@gmail.com>
2025-10-21 15:10:29 -0300

    :sparkles: aula javascript

---
+++ introducaojs/script.js
@@ -0,0 +1,38 @@
+console.log("Olá Mundo!");
+// o console é uma uma ferramenta do desenvolvedor que existem nos navegadores, para debugar erros e ver mensagens
+
+//variaveis
+// São usadas para armazenar os dados do usuarios
+
+// tipos de variaveis:
+// var, let e const
+
+//estruturada basica da variavel:
+//tipo de variavel + nome da variavel + valor;
+let idade = 18;
+console.log(idade);
+idade = 45;
+console.log(idade);
+//let é uma variavel que podemos reatribuir o valor
+let nome = "Maria Carolina";
+console.log(nome);
+
+//tipos de dados
+// textuais => palavra, caractere, frases e textos (strings "",'')
+let cidade = 'Mim morar "Ribeirão"';
+console.log(cidade)
+
+//numerico => number
+let quantidadeDeBolacha = 20;
+// quantidade de bolacha
+//camelCase
+let quantidadeDeCafe = 1;
+
+
+console.log(quantidadeDeBolacha + quantidadeDeCafe)
+let n1 = 1;
+let n2 = 3;
+
+console.log(n1 + n2)// trouxe o valor 4
+n2 = "3"
+console.log(n1 + n2);
